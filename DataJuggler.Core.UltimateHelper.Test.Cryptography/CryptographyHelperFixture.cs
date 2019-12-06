28d79f975da5c2cce1704c0d2338ce2c8b04309a
Data Juggler <corby@datajuggler.com>
2019-12-06 01:19:19 -0600

    Add a Test back for Cryptography Helper.

---
+++ DataJuggler.Core.UltimateHelper.Test.Cryptography/CryptographyHelperFixture.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataJuggler.Core.UltimateHelper.Test.Cryptography
+{
+    [TestClass]
+    public class CryptographyHelperFixture
+    {
+        [TestMethod]
+        public void TestEncryptionDecryption()
+        {
+            var message = "Some string that has a ↈ weird character in it.";
+            var password = "crypto1234";
+
+            var encrypted = CryptographyHelper.EncryptString(message, password);
+            var decrypted = CryptographyHelper.DecryptString(encrypted, password);
+
+            Assert.AreEqual(message, decrypted);
+        }
+    }
+}
